170a33908b59e6fba82ff54fac9c3d0f8ec19e8e
Marcio Roberto <mroberto@gmail.com>
2020-04-21 19:53:12 -0300

    added rest controller

---
+++ src/main/java/com/base/coronavirustracker/resource/CoronaResource.java
@@ -0,0 +1,26 @@
+package com.base.coronavirustracker.resource; 
+
+import java.util.List;
+
+import com.base.coronavirustracker.model.LocationStats;
+import com.base.coronavirustracker.services.CoronaVirusDataService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/cases")
+public class CoronaResource {
+
+    @Autowired
+    private CoronaVirusDataService service;
+
+    @GetMapping()
+    public ResponseEntity<List<LocationStats>> findAll() {
+        List<LocationStats> listDto = service.getAllStats();
+        return ResponseEntity.ok().body(listDto);
+    }
+}
